c6348a23536fb1f6edacb253ad76900ae37b456f
Ivan Molodetskikh <yalterz@gmail.com>
2018-01-27 20:28:12 +0300

    [sys] Re-run build.rs if VAPOURSYNTH_LIB_DIR changes

--- vapoursynth-sys/build.rs
+++ vapoursynth-sys/build.rs
@@ -1,12 +1,19 @@
 use std::env;
 
+const LIBRARY_DIR_VARIABLE: &str = "VAPOURSYNTH_LIB_DIR";
+
 fn main() {
+    // Make sure the build script is re-run if our env variable is changed.
+    println!("cargo:rerun-if-env-changed={}", LIBRARY_DIR_VARIABLE);
+
     let windows = env::var("TARGET").unwrap().contains("windows");
 
-    if let Ok(dir) = env::var("VAPOURSYNTH_LIB_DIR") {
+    // Library directory override.
+    if let Ok(dir) = env::var(LIBRARY_DIR_VARIABLE) {
         println!("cargo:rustc-link-search=native={}", dir);
     }
 
+    // Handle linking to VapourSynth libs.
     if env::var("CARGO_FEATURE_VAPOURSYNTH_FUNCTIONS").is_ok() {
         println!("cargo:rustc-link-lib=vapoursynth");
     }
