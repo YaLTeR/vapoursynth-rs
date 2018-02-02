f789ce0e7f6a08f707d21e638f9f05521538f1ff
Ivan Molodetskikh <yalterz@gmail.com>
2018-02-02 10:32:26 +0300

    Test on OSX

--- .travis/install-vapoursynth.sh
+++ .travis/install-vapoursynth.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 set -ex
 
-sudo add-apt-repository -y ppa:djcj/vapoursynth
-sudo apt-get update -qq
-sudo apt-get install -qq vapoursynth
-sudo ln -s /usr/lib/x86_64-linux-gnu/libvapoursynth-script.so{.0,}
+if [[ $TRAVIS_OS_NAME == 'osx' ]]; then
+	brew install vapoursynth
+else
+	sudo add-apt-repository -y ppa:djcj/vapoursynth
+	sudo apt-get update -qq
+	sudo apt-get install -qq vapoursynth
+	sudo ln -s /usr/lib/x86_64-linux-gnu/libvapoursynth-script.so{.0,}
+fi
