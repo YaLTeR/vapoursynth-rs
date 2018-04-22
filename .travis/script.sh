409419b86efd0c5332f2d5d6793a285bd2239e92
Ivan Molodetskikh <yalterz@gmail.com>
2018-04-22 11:10:00 +0300

    Attempt to fix Travis

--- .travis/script.sh
+++ .travis/script.sh
@@ -1,17 +1,6 @@
 #!/bin/sh
 set -ex
 
-# Build and test with no features.
-cargo build --verbose
-cargo test --verbose
-
-# Build with all features.
-cargo build --verbose --all-features
-
-# Doc with all features.
-cargo doc --verbose --all-features
-cp .travis/index.html target/doc/
-
 # Test with all possible combinations of all features.
 cd vapoursynth; python3 ../.travis/run-tests.py
 cd ..
@@ -21,3 +10,8 @@ cd sample-plugin
 cargo build --verbose
 cargo run --verbose --bin test \
     --features "cfg-if vapoursynth/vapoursynth-functions vapoursynth/vsscript-functions"
+cd ..
+
+# Doc with all features.
+cargo doc --verbose --all-features
+cp .travis/index.html target/doc/
