6ace1aeb1c75925ad7f9278e47ebe173da46310d
Ivan Molodetskikh <yalterz@gmail.com>
2022-01-18 13:22:43 +0300

    CI: Use VS 53 on Windows

--- build/windows-script.ps1
+++ build/windows-script.ps1
@@ -14,13 +14,13 @@ If ($arch -eq "i686") {
 }
 
 # Download Python embeddable and VapourSynth portable
-$VS_PATH = "https://github.com/vapoursynth/vapoursynth/releases/download/R57"
+$VS_PATH = "https://github.com/vapoursynth/vapoursynth/releases/download/R53"
 curl -LO "https://www.python.org/ftp/python/3.9.10/$PYTHON_PKG"
-curl -LO "$VS_PATH/VapourSynth$SUFFIX-Portable-R57.7z"
+curl -LO "$VS_PATH/VapourSynth$SUFFIX-Portable-R53.7z"
 
 # Unzip Python embeddable and VapourSynth portable
 7z x "$PYTHON_PKG" -o"$PY_DIR"
-7z x "VapourSynth$SUFFIX-Portable-R57.7z" -o"$VS_DIR"
+7z x "VapourSynth$SUFFIX-Portable-R53.7z" -o"$VS_DIR"
 
 # Move all VapourSynth files inside the Python ones
 Move-Item -Force -Path "$VS_DIR\*" -Destination "$PY_DIR"
